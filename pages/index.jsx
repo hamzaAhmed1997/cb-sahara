df1084ea5c6ff84caac3b22a522dc3bdc5ed1576
mtahirgulzar <tahir.gulzar@vigorant.com>
2023-01-20 11:55:20 +0500

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+
+import Layout from "@/src/components/Layout";
+ 
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-3xl text-red-300 ">HomePAge</h1>
+      </Layout>
+    </>
+  );
+}
