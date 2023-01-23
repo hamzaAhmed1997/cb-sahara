b03dc093e48b6977b99573b131f811911c86ffba
Tahira-Malik <85984818+Tahiraawan@users.noreply.github.com>
2023-01-23 15:46:29 +0500

    Added navbar

---
+++ src/components/Navbar/TopCard/index.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Link from 'next/link';
+import Facebook from '../../Common/Icon/Facebook';
+import Twitter from '../../Common/Icon/Twitter';
+import Google from '../../Common/Icon/Google';
+// import Instagram from '../../Common/Icon/Instagram'
+// import Youtube  from '../../Common/Icon/Youtube';
+
+const TopCard = () => {
+  return (
+    <div className="w-full  px-4 py-1 bg-[#FFC648]  ">
+     <div className='flex items-center justify-between max-w-[1140px] mx-auto px-4  '>
+     <div className="w-[21%]">
+        <div className="flex gap-x-[2px] space-x-2  py-3 items-start  w-full md:w-auto md:items-center">
+        <Link href={"/"} legacyBehavior>
+              <a className="hover:text-red-600">
+                <Facebook/>
+              </a>
+            </Link>
+        <Link href={"/"} legacyBehavior>
+              <a className="">
+                <Twitter/>
+              </a>
+            </Link>
+        <Link href={"/"} legacyBehavior>
+              <a className="">
+                <Google/>
+              </a>
+            </Link>
+        <Link href={"/"} legacyBehavior>
+              <a className="">
+                {/* <Instagram/> */}
+              </a>
+            </Link>
+        <Link href={"/"} legacyBehavior>
+              <a className="">
+                {/* <Youtube/> */}
+              </a>
+            </Link>
+        </div>
+      </div>
+      <div className="w-[21%]">
+        <div className="flex gap-x-[2px] space-x-2  py-3 justify-end w-ful md:w-auto md:items-center">
+        <Link href={"/"} legacyBehavior>
+              <a className="">
+                <Facebook/>
+              </a>
+            </Link>
+        <Link href={"/"} legacyBehavior>
+              <a className="">
+                <Twitter/>
+              </a>
+            </Link>
+     
+        </div>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default TopCard
--- src/components/Navbar/subMenu/index.jsx
+++ src/components/Navbar/subMenu/index.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import styles from "../style.module.css";
 import Link from "next/link"
-
-
-const SubMenu = ({setIsSubmenu,dropdown,service,condition}) => { 
+const SubMenu = ({dropdown,setIsSubmenu}) => { 
     return (
         <>
             <div
                 onMouseEnter={() => setIsSubmenu(true)}
                 onMouseLeave={() => setIsSubmenu(false)}
-                className={`absolute top-[30px] left-[0px]  w-[170px] transition-all duration-3000 ease  z-50 bg-white`}
+                className={`absolute top-[30px] left-[0px]  w-[150px] transition-all duration-3000 ease  z-50 bg-white`}
             >
                 <ul className={` bg-white`}>
                     {dropdown?.map((item, index) => (
-                        <Link key={index} href={`${service ? `/services/${item.attributes.slug}` : condition ?`/conditions-treated/${item.attributes.slug}` : `${item.link}`}`}>
+                        <Link key={index} href={"/"} legacyBehavior >
                             <a>
                                 <li
-                                    className={`px-[20px] py-[13px] transition-all ease duration-500 ${styles.cusHover}`}
+                                    className={`px-[20px] py-[13px] transition ${styles.cusHover}`}
                                 >
                                     <p
-                                        className={`${styles.hoverText} transition-all ease duration-500 text-[12px] leading-[15px]`}
+                                        className={`${styles.hoverText} transition font-600 text-[15px] leading-[15px]`}
                                     >
                                         {item.title || item.attributes.title}
                                     </p>
