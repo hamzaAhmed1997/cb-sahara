151319b2a3c1fe7c0eda54b43d618015446ad88d
Tahira-Malik <85984818+Tahiraawan@users.noreply.github.com>
2023-01-25 18:05:35 +0500

    cards

---
+++ src/components/HomePage/FlipCards/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+const data = [
+  {
+    img: "https://saharamkt.com/wp-content/uploads/2020/01/Grocey-20_1559b48e547c3469bba690740fc13775.png",
+    name: "tahira",
+    description: "Sahara Restaurant offers the most delirious Persian and Afghani cuisine in a friendly and welcoming environment. All foods are prepared in a home-made style with our best Halal meat, chicken, and other ingredients.",
+  },
+  {
+    img: "https://saharamkt.com/wp-content/uploads/2020/01/Grocey-20_1559b48e547c3469bba690740fc13775.png",
+    name: "tahira",
+    description: "Sahara Restaurant offers the most delirious Persian and Afghani cuisine in a friendly and welcoming environment. All foods are prepared in a home-made style with our best Halal meat, chicken, and other ingredients.",
+  },
+  {
+    img: "https://saharamkt.com/wp-content/uploads/2020/01/Grocey-20_1559b48e547c3469bba690740fc13775.png",
+    name: "tahira",
+    description: "Sahara Restaurant offers the most delirious Persian and Afghani cuisine in a friendly and welcoming environment. All foods are prepared in a home-made style with our best Halal meat, chicken, and other ingredients.",
+  },
+  {
+    img: "https://saharamkt.com/wp-content/uploads/2020/01/Grocey-20_1559b48e547c3469bba690740fc13775.png",
+    name: "tahira",
+    description: "Sahara Restaurant offers the most delirious Persian and Afghani cuisine in a friendly and welcoming environment. All foods are prepared in a home-made style with our best Halal meat, chicken, and other ingredients.",
+  },
+  {
+    img: "https://saharamkt.com/wp-content/uploads/2020/01/Grocey-20_1559b48e547c3469bba690740fc13775.png",
+    name: "tahira",
+    description: "Sahara Restaurant offers the most delirious Persian and Afghani cuisine in a friendly and welcoming environment. All foods are prepared in a home-made style with our best Halal meat, chicken, and other ingredients.",
+  },
+  {
+    img: "https://saharamkt.com/wp-content/uploads/2020/01/Grocey-20_1559b48e547c3469bba690740fc13775.png",
+    name: "tahira",
+    description: "Sahara Restaurant offers the most delirious Persian and Afghani cuisine in a friendly and welcoming environment. All foods are prepared in a home-made style with our best Halal meat, chicken, and other ingredients.",
+  },
+];
+
+const FlipCards = () => {
+  return (
+    <div className="w-full my-[30px]">
+      <div className=" max-w-[1140px] w-full mx-auto px-4">
+        <div className="w-full mx-auto grid grid-cols-1 gap-4 lg:grid-cols-3">
+          {data.map((item, index) => {
+            return (
+              <div key={index}>
+                <div class="flip-cards w-[379px]">
+                  <div class="flip-cards-inner p-[10px">
+                    <div class="flip-cards-front mb-[20px]">
+                      <img src={item.img} alt="" />
+                    </div>
+                    <div class="flip-cards-back text-center p-[35px] flex flex-col justify-center bg-[#6A0000] text-[#ffff]">
+                      <h2 className="mb-[20px]">{item.name}</h2>
+                      <p className="text-14px"> {item.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlipCards;
---
+++ src/components/HomePage/ServicesCards/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import CardItem from "./CardItem";
+
+const data = [
+  {
+    title: "Restaurant",
+    img: "https://saharamkt.com/wp-content/uploads/2022/03/restaurant.jpg",
+  },
+  {
+    title: "Restaurant",
+    img: "https://saharamkt.com/wp-content/uploads/2022/03/restaurant.jpg",
+  },
+  {
+    title: "Restaurant",
+    img: "https://saharamkt.com/wp-content/uploads/2022/03/restaurant.jpg",
+  },
+];
+
+const ServicesCards = () => {
+  return (
+    <div className=" pt-[30px] pb-[20px] ">
+     <div className="py-[60px]">
+     <div className="mb-[50px]">
+        <p className="text-center text-[#FFB400] text-[30px] font-600 leading-[1]  ">
+          Order Online
+        </p>
+        <h2 className="text-center pb-2 text-[#FFB400] text-[30px] sm:text-[40px] font-bold leading-[1.2] mt-[8px] mb-[16px] ">
+          Convenient Pickup and Delivery Services
+        </h2>
+      </div>
+      <div className="max-w-[1300px] mx-auto px-4  grid lg:grid-cols-3 md:grid-cols-2 gap-[22px]">
+        {data.map((data, index) => {
+          return (
+            <div key={index}>
+              <CardItem title={data.title} link={data.link} image={data.img} />
+            </div>
+          );
+        })}
+      </div>
+     </div>
+    </div>
+  );
+};
+
+export default ServicesCards;
