a409ff58ca7373ecc7d534c18ec0e88bb533ce85
mtahirgulzar <tahir.gulzar@vigorant.com>
2023-01-26 20:17:24 +0500

    before set navbar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,7 +3,7 @@ import Footer from '../Footer'
 import Header from '../Header'
 import Navbar from '../Navbar'
 
-export default function Layout({children}) {
+export default function Layout({children, data}) {
   return (
     <div>
       <Header/>
