4c89c93d2ac950611df134724f250698a5d4424b
mtahirgulzar <tahir.gulzar@vigorant.com>
2023-01-27 15:24:36 +0500

    Client before

--- src/components/Common/Icon/Profile.jsx
+++ src/components/Common/Icon/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Profile = ({props}) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none" stroke-linecap="round" strokeLinejoin="round">
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <circle cx="12" cy="7" r="4" />
     <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
