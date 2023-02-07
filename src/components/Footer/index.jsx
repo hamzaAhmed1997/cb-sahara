16c4adc7e8eeed0313e2c1383877febe416c8dc9
mtahirgulzar <tahir.gulzar@vigorant.com>
2023-02-07 17:19:13 +0500

    nav changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -154,7 +154,7 @@ export default function Footer({ data }) {
       <div className="max-w-[1140px] px-4 mx-auto h-[1px] bg-white mt-[30px] " />
       <div className="">
         <div className="text-center text-[14px] font-[400] text-white py-[20px] px-4">
-          {year}
+        © {year}
           <span className="hover:text-[#FFC648] transition-all duration-300 font-[500]">
             <Link href={`${data?.links?.companylink}`} legacyBehavior>
               <a href={`${data?.links?.companylink}`}>
